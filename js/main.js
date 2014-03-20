eefc6ee503c669ef26db49596711d42eb70ca8ab
ahernandezfriz <a.hernandez.friz@gmail.com>
2014-03-20 17:24:19 -0300

    archivo con funciones especificas para la plantilla

---
+++ js/main.js
@@ -0,0 +1,11 @@
+jQuery(function($){
+
+/*
+ * Quita el atributo href de los enlaces
+ * cuyo li es padre de un ul	
+------------------------------------------------------------------------------*/
+    $('.menu li').has('ul.children').css('cursor', 'pointer');
+    $('.menu li').has('ul').children("a").removeAttr('href'); 
+});
+
+
